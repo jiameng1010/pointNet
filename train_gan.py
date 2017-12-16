ee58b7caffb54652f5e9e35adcd29bbe34ffadf1
jiameng1010 <jiameng1010@qq.com>
2017-12-16 14:50:54 -0500

    gan

--- train_gan.py
+++ train_gan.py
@@ -8,8 +8,6 @@ import importlib
 import h5py
 import os
 import sys
-
-from transform_nets import input_transform_net, feature_transform_net
 from tensorflow.python.ops import variable_scope
 from tensorflow.contrib.gan.python.losses.python import tuple_losses_impl as tfgan_losses
 from tensorflow.contrib.data import Dataset, Iterator
@@ -19,6 +17,8 @@ sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, 'models'))
 sys.path.append(os.path.join(BASE_DIR, 'utils'))
 import tf_util
+from transform_nets import input_transform_net, feature_transform_net
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
 parser.add_argument('--model', default='pointnet_cls', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
