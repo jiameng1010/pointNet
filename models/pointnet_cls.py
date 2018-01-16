fb43a099c78d1b39b8c522be37b659ffa4561d64
jiameng1010 <jiameng1010@qq.com>
2018-01-15 23:23:40 -0500

    gan

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -149,14 +149,14 @@ def get_model_rbf3(point_cloud, is_training, bn_decay=None):
     sub_bias = tf.tile(sub_bias, [batch_size, 1024, 1, 1, 1])
     sub_feature = tf.tile(tf.expand_dims(point_cloud_transformed, 4), [1, 1, 1, c2, c1])
     #sub_net = tf.exp(-tf.square(tf.norm(tf.subtract(sub_feature, sub_bias), axis=2, keep_dims=True)))
-    sub_net = tf.squeeze(tf.exp(-tf.norm(tf.subtract(sub_feature, sub_bias), ord=4, axis=2, keep_dims=True)))
+    sub_net = tf.squeeze(tf.exp(-tf.square(tf.norm(tf.subtract(sub_feature, sub_bias), axis=2, keep_dims=True))))
     sub_net = tf.transpose(sub_net, perm=[0, 1, 3, 2])
     sub_net = tf_util.max_pool2d(sub_net, [num_point,1], stride=[1, 1],
                              padding='VALID', scope='maxpool')
-    #sub_net = tf_util.conv2d(sub_net, 16, [1,1],
-    #                             padding='VALID', stride=[1,1],
-    #                             bn=True, is_training=is_training,
-    #                             scope='mini_conv1', bn_decay=bn_decay)
+    sub_net = tf_util.conv2d(sub_net, 16, [1,1],
+                                 padding='VALID', stride=[1,1],
+                                 bn=True, is_training=is_training,
+                                 scope='mini_conv1', bn_decay=bn_decay)
     sub_net = tf_util.conv2d(sub_net, 4, [1,1],
                                  padding='VALID', stride=[1,1],
                                  bn=True, is_training=is_training,
@@ -184,11 +184,11 @@ def get_model_rbf3(point_cloud, is_training, bn_decay=None):
     #                              scope='fc0', bn_decay=bn_decay)
     #net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
     #                      scope='dp1')
-    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
+    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
                                   scope='fc1', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
                           scope='dp1')
-    net = tf_util.fully_connected(net, 128, bn=True, is_training=is_training,
+    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
                                   scope='fc2', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
                           scope='dp2')
