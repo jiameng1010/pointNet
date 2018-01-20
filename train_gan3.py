a51acd2131853f2253b36b23867db7d3c16349f4
jiameng1010 <jiameng1010@qq.com>
2018-01-20 03:13:46 -0500

    gan

--- train_gan3.py
+++ train_gan3.py
@@ -473,7 +473,7 @@ def train():
             #    if (acc > 0.5) or (repeat == 6):
             #        break
             if epoch == 0:
-                for i in range(2):
+                for i in range(5):
                     trainD(sess, sess2, ops, train_writer)
             else:
                 if epoch%10 == 0:
