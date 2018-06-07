0232a7658d4f2e9984a0a9eef7ff9ecdc720f85e
jiameng1010 <jiameng1010@qq.com>
2018-06-07 17:04:32 -0400

    gan

--- train_mini_network.py
+++ train_mini_network.py
@@ -179,13 +179,13 @@ def train():
             net1 = tf.constant(random_weights, dtype=tf.float32)
             #net1 = tf.constant(np.random.normal(size=(3, 3072)), dtype=tf.float32)
             pred, end_points, G_features, pred_elm_weight = MODEL.get_model_field(pointclouds_pl, probe_points_pl, is_training_pl, net1, bn_decay=bn_decay)
-
+            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=tf.concat([pred, -pred], axis=2), labels=labels_pl)
             pred = tf.squeeze(pred, axis=2)
             loss1 = tf.reduce_mean(tf.losses.mean_squared_error(labels=labels_pl, predictions=pred))
             loss2 = 1e-9 * tf.reduce_mean(tf.losses.mean_squared_error(labels=elm_weight, predictions=pred_elm_weight))
             rate = 1e-1
             #loss = loss1
-            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=labels_pl)
+
             tf.summary.scalar('loss', loss)
             loss_rate = tf.divide(loss2, loss1)
 
